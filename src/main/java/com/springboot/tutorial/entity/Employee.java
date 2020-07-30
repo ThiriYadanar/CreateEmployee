4298fcd569c42292261c09bb7068bce90c4cae98
Thiri <thiri@Thiris-MacBook-Air.local>
2020-07-30 21:52:35 +0630

    database name changed

--- src/main/java/com/springboot/tutorial/entity/Employee.java
+++ src/main/java/com/springboot/tutorial/entity/Employee.java
@@ -2,7 +2,6 @@ package com.springboot.tutorial.entity;
 
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,20 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Table(name="thiri")
+@Table(name="employee")
 @Data
 public class Employee {
 	@Id
